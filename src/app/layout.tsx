aaa9547d59bb39ff3644d92afd046ab848f3ab5c
ricardo <ricardotsopse@gmail.com>
2023-10-06 12:26:30 +0100

    some changes made

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import HeaderTop from "../components/HeaderTop";
+import HeaderMain from "../components/HeaderMain";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <HeaderTop/>
+        <HeaderMain/>
         {children}
         </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <main>Hello</main>
+    <main className="container">Hello</main>
   )
 }
