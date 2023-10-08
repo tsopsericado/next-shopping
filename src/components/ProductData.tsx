8556fcf87d0bbb08eb6c2810fb5c55c57bd74cfe
ricardo <ricardotsopse@gmail.com>
2023-10-08 15:41:51 +0100

    some changes made

--- src/components/ProductData.tsx
+++ src/components/ProductData.tsx
@@ -135,7 +135,7 @@ const ProductData: React.FC<propsType> = ({
           alt={title}
         />
       </div>
-      <div className="">
+      <div className="space-y-2 py-2">
         <h2 className="text-accent font-medium uppercase">{title}</h2>
         <p className="text-gray-500 max-w-[150px]">{desc}</p>
         <div>{generateRating(rating)}</div>
@@ -143,7 +143,6 @@ const ProductData: React.FC<propsType> = ({
 
         <div className="font-bold flex gap-4">
             ${price}
-            <del className="text-gray-500 font-normal">${parseInt(price) + 50}.00</del>
         </div>
       </div>
     </div>
